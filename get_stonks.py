cfc46799875067b471e1fc0aa39811aa61a10c3f
polishchuker <74683365+polishchuker@users.noreply.github.com>
2021-01-06 10:34:37 -0500

    Update get_stonks.py

--- get_stonks.py
+++ get_stonks.py
@@ -4,12 +4,17 @@ import yfinance as yf
 
 def get_portfolio():
     num_of_stocks = int(input('How many stocks do you have? '))
-
+    print('In the following format  in the following format "year-month-day" e.g: 2009-01-13  ')
+    start_date = input('Enter start date of data: ')
+    print()
+    print('In the following format  in the following format "year-month-day" e.g: 2020-04-20  ')
+    end_date= input('Enter the end date of data: ')
+    print()
     portfolio = []
     for stock in range(num_of_stocks):
         stock = input('Enter Ticker Symbol of stock: ').upper()
         stock_data = yf.Ticker(stock)
-        stock_history = stock_data.history(start="2009-01-01", end="2020-01-01")
+        stock_history = stock_data.history(start=start_date, end=end_date)
 
         #checks to see if ticker is valid, if not raise exception
         if stock_history.empty:
